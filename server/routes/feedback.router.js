233a308fb2dc07ad2ce8deebd35e2019410d3bac
Rider <goejrider@gmail.com>
2022-10-28 15:23:11 -0500

    Make router.post to add feedback to database

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,5 +3,12 @@ const router =  express.Router();
 
 const pool = require('../modules/pool');
 
+router.post('/', (req,res)=>{
+    let newFeedback = req.body;
+    const sqlText = `
+    INSERT INTO "
+    `
+})
+
 
 module.exports = router;
